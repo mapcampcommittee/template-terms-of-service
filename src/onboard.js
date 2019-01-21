caed6f4ff7521f4ab4a3980e39fde4c9174d7834
Benjamin Mosior <bemosior@gmail.com>
2019-01-21 13:25:02 -0500

    Remove extra space in message.

--- src/onboard.js
+++ src/onboard.js
@@ -18,7 +18,7 @@ const message = {
   attachments: JSON.stringify([
     {
       title: 'Helpful Resources',
-      text: 'Just to make you aware, there\'s a <https://community.wardleymaps.com|public Wardley maps forum> and a <https://www.linkedin.com/groups/13604539|Wardley Maps Community LinkedIn group> that you\'d also be welcome to join.\n\nFinally, a few more resources you may find useful:\n* <https://medium.com/wardleymaps|Creative commons book by Simon Wardley>\n* <https://bit.ly/wardleymaps|YouTube channel>\n* <https://www.wardleymaps.com/tool.html|Atlas>\n* <https://hiredthought.com/wardley-mapping/|Additional tools and templates>\n* <https://learn.leadingedgeforum.com/p/wardley-mapping/?product_id=277424|LEF Course>\n*  <https://www.map-camp.com/_pages/code_of_conduct/|Our Code of Conduct>',
+      text: 'Just to make you aware, there\'s a <https://community.wardleymaps.com|public Wardley maps forum> and a <https://www.linkedin.com/groups/13604539|Wardley Maps Community LinkedIn group> that you\'d also be welcome to join.\n\nFinally, a few more resources you may find useful:\n* <https://medium.com/wardleymaps|Creative commons book by Simon Wardley>\n* <https://bit.ly/wardleymaps|YouTube channel>\n* <https://www.wardleymaps.com/tool.html|Atlas>\n* <https://hiredthought.com/wardley-mapping/|Additional tools and templates>\n* <https://learn.leadingedgeforum.com/p/wardley-mapping/?product_id=277424|LEF Course>\n* <https://www.map-camp.com/_pages/code_of_conduct/|Our Code of Conduct>',
       color: '#74c8ed',
     }]
   ),
