e8d430ce55c26a2676076ec1878b057cd477872c
Benjamin Mosior <bemosior@gmail.com>
2019-01-21 13:15:48 -0500

    carve out data-related functions

--- src/onboard.js
+++ src/onboard.js
@@ -25,52 +25,16 @@ const message = {
 };
 
 const initialMessage = (teamId, userId) => {
-  let data = false;
-  // try fetch team/user pair. This will throw an error if nothing exists in the db
-  try { data = db.getData(`/${teamId}/${userId}`); } catch (error) {
-    console.error(error);
-  }
-
-  // `data` will be false if nothing is found or the user hasn't accepted the ToS
-  if (!data) {
-    // add or update the team/user record
-    db.push(`/${teamId}/${userId}`, false);
-
-    // send the default message as a DM to the user
-    message.channel = userId;
-    axios.post(`${apiUrl}/chat.postMessage`, qs.stringify(message))
-      .then((result => {
-        console.log(result.data);
-      }));
-  } else {
-    console.log('Already onboarded');
-  }
+  // send the default message as a DM to the user
+  message.channel = userId;
+  axios.post(`${apiUrl}/chat.postMessage`, qs.stringify(message))
+    .then((result => {
+      console.log(result.data);
+    }));
 };
 
-// set the team/user record to true to indicate that they've accepted the ToS
-// you might want to store the date/time that the terms were accepted
+const accept = (userId, teamId) => {};
 
-const accept = (userId, teamId) => db.push(`/${teamId}/${userId}`, true);
-
-// find all the users who've been presented the ToS and send them a reminder to accept.
-// the same logic can be applied to find users that need to be removed from the team
-const remind = () => {
-  try {
-    const data = db.getData('/');
-    Object.keys(data).forEach((team) => {
-      Object.keys(data[team]).forEach((user) => {
-        if (!data[team][user]) {
-          message.channel = user;
-          message.text = 'REMINDER';
-
-          axios.post(`${apiUrl}/chat.postMessage`, qs.stringify(message))
-          .then((result => {
-            console.log(result.data);
-          }));
-        }
-      });
-    });
-  } catch (error) { console.error(error); }
-};
+const remind = () => {};
 
 module.exports = { initialMessage, accept, remind };
